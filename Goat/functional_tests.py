df52d9fe0d9b0108fedbd2ff74d391b9dbb65bc5
neilwilliams36 <nwilliams36@yahoo.com>
2014-11-04 12:29:06 +0930

    first commit

---
+++ Goat/functional_tests.py
@@ -0,0 +1,47 @@
+__author__ = 'nwilliams1'
+
+from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        #Edith has heard about a coll new online to-do app.
+        #she goes to check out the homepage
+        #added a change here
+        self.browser.get('http://127.0.0.1:8000')
+        #She notices the page title and header mention to-do list
+        self.assertIn('To-Do List', self.browser.title)
+        self.fail('Finish the test')
+
+        #she is invited to enter a to-do item straight away
+
+        #she type "Buy peacock feathers" into a text box
+
+        #she hit enter and the page updates
+        #Now the page lists "1. Buy peacock feathers" as an item on the to-do list
+
+        #There is still a text box inviting her to add another item
+        #She enters "Use peacock feathers to make a fly"
+
+        #The page updates and shows both items
+
+        #Edith wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
+
+
+        # She visits that URL - her to-do list is still there.
+
+
+        # Satisfied, she goes back to sleep
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
