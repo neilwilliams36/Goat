2c7664450c6bb866f67a8959bc311b36824236d8
neilwilliams36 <nwilliams36@yahoo.com>
2014-11-04 13:26:47 +0930

    all

---
+++ lists/tests.py
@@ -0,0 +1,17 @@
+from django.http import HttpRequest
+from django.test import TestCase
+from django.core.urlresolvers import resolve
+from django.template.loader import render_to_string
+
+from lists.views import home_page
+
+class HomePageTest(TestCase):
+    def test_root_URL(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest() #
+        response = home_page(request) #
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
