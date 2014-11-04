f84ac4bf2e08eb502f3895ff3d702731af67ae03
neilwilliams36 <nwilliams36@yahoo.com>
2014-11-04 14:18:09 +0930

    Added database model

--- Goat/functional_tests.py
+++ Goat/functional_tests.py
@@ -3,6 +3,7 @@ __author__ = 'nwilliams1'
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -12,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         #Edith has heard about a coll new online to-do app.
@@ -32,15 +38,21 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys("Buy peacock feathers")
         #she hit enter and the page updates
         inputbox.send_keys(Keys.ENTER)
+        #time.sleep(10)
         #Now the page lists "1. Buy peacock feathers" as an item on the to-do list
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1. Buy Peacock feathers' for row in rows), "New to-do item did not appear in table")
+        self.check_for_row_in_list_table('1. Buy peacock feathers')
         #There is still a text box inviting her to add another item
         #She enters "Use peacock feathers to make a fly"
-
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
         #The page updates and shows both items
-
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.check_for_row_in_list_table('1. Buy peacock feathers')
+        self.check_for_row_in_list_table('2. Use peacock feathers to make a fly')
         #Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
