414f14e9834d9a5fbe64835ed6d163d16a9f9f42
Neil Williams <nwilliams36@yahoo.com>
2014-11-05 11:52:26 +0930

    added list model

---
+++ lists/migrations/0003_auto_20141105_1128.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('lists', '0002_item_text'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='List',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('list', models.TextField(default='')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='item',
+            name='list',
+            field=models.TextField(default=''),
+            preserve_default=True,
+        ),
+    ]
