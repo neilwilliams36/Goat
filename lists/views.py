e1da45384f339563b78eed6c61a805ac3cbba5df
Neil Williams <nwilliams36@yahoo.com>
2014-11-11 09:14:13 +0930

    Pythonanywhere3

--- lists/views.py
+++ lists/views.py
@@ -14,6 +14,7 @@ def new_list(request):
     list_ = List.objects.create()
     Item.objects.create(text = request.POST['item_text'], list = list_)
     return redirect('/lists/%d/' % (list_.id))
+    #nothing
 
 def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
