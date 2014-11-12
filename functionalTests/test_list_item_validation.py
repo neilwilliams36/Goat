89e806e34f48aea41597085d45ffedc6faf4f456
Neil Williams <nwilliams36@yahoo.com>
2014-11-12 10:53:19 +0930

    Refactoring to page 190 Chapter 10)

--- functionalTests/test_list_item_validation.py
+++ functionalTests/test_list_item_validation.py
@@ -10,16 +10,32 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         #edith goes to the home page and accidentally tries to submit and empty list.
         #She hits Enter on the input box
+        if self.server_url[7:12] == 'local':
+            self.browser.get("http://127.0.0.1:8000")
+        else:
+            self.browser.get(self.server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys(Keys.ENTER)
 
         #The home page refreshes, and there is an error message say that the list cannot be blank
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         #She tries again with some text in the item, which now works
-
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy Milk \n')
+        self.check_for_row_in_list_table("1. Buy Milk")
         #Perversely, she now decides to submit a second blank list
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys(Keys.ENTER)
 
         #She receives a similar warning on the list page
-
-        #And she corrects is by filling n some text
-
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
+        #And she corrects is by filling in some text
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy Cheese \n')
+        self.check_for_row_in_list_table("1. Buy Milk")
+        self.check_for_row_in_list_table("2. Buy Cheese")
         #she is finished
         self.fail('Finish the test')
--- lists/views.py
+++ lists/views.py
@@ -1,22 +1,34 @@
+from django.core.exceptions import ValidationError
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from lists.models import Item, List
+
+
 
 def home_page(request):
     return render (request, 'home.html')
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    items = Item.objects.filter(list=list_)
-    return render (request, 'lists.html', {'list': list_ })
+    error = None
+    if request.method == 'POST':
+        try:
+            item = Item(text=request.POST['item_text'], list=list_)
+            item.full_clean()
+            item.save()
+            return redirect(list_)
+        except ValidationError:
+            error = "You can't have an empty list item"
+    return render (request, 'lists.html', {'list': list_ , 'error': error})
 
 def new_list(request):
     list_ = List.objects.create()
-    Item.objects.create(text = request.POST['item_text'], list = list_)
-    return redirect('/lists/%d/' % (list_.id))
-    #nothing
+    item = Item.objects.create(text = request.POST['item_text'], list = list_)
+    try:
+        item.full_clean()
+    except ValidationError:
+        error = "You can't have an empty list item"
+        list_.delete()
+        return render(request, 'home.html', {'error': error})
+    return redirect(list_)
 
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text = request.POST['item_text'], list = list_)
-    return redirect('/lists/%d/' % (list_.id))
